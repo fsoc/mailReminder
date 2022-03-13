599b03a17d5ae6cf69c9331eb858cb2bfb1ab8b1
fsoc <fsoc@null>
2022-03-13 17:53:00 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# mailReminder
+This application should preferrebly be automated with systemd or cron to run often.
+It will email a message with the selected topic to specific emails, it adds random prefix and suffixes to the message body to sound more authentic.
+
+It will solve your problems of having to remind someone to answer.
+Just copy the data.ini and msg.ini examples and run the program.
---
+++ mailReminder.py
@@ -0,0 +1,28 @@
+import configparser, os, subprocess, random
+
+def getMsg():
+    data = configparser.ConfigParser(allow_no_value=True)
+    data.read('msg.ini')
+
+    s = random.choice(list(data['suffix'].keys()))
+    p = random.choice(list(data['prefix'].keys()))
+    return [p,s]
+
+
+def runData():
+    data = configparser.ConfigParser()
+    data.read('data.ini')
+    getMsg()
+    
+    for section in data.sections():
+        s = data[section]
+        mail(s['from'], s['to'], s['sub'], section)
+
+
+def mail(fr, to, sub, extraMsg):
+    final_message = '%s %s. %s' % (getMsg()[0], extraMsg, getMsg()[1])
+    command_line = 'echo "%s"|mail -s "%s" -r %s %s' % (final_message, sub, fr, to)
+    subprocess.Popen(command_line, shell=True)
+    print('mail sent to %s m:%s s:%s' % (to ,final_message, sub))
+
+runData()
